69826b9bcaddc698c9b9cc7b3c9fc38639929948
Marcin Wegrzynek <121108398+Marwgithub@users.noreply.github.com>
2023-01-24 15:55:24 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -75,8 +75,4 @@ if streamlit.button('Add a Fruit to the list'):
 #fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 #fruits_to_show = my_fruit_list.loc[fruits_selected]
 #streamlit.dataframe(fruits_to_show)
-streamlit.header("Add a fruit")
-fruit_choice = streamlit.text_input('What fruit would you like to add?')
-streamlit.write('Thanks for adding: ', fruit_choice)
 
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
