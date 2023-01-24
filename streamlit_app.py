31c64a14dc67ea02408dfe30a16e5b2e2709fce6
Marcin Wegrzynek <121108398+Marwgithub@users.noreply.github.com>
2023-01-24 15:36:03 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,6 +48,18 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
 streamlit.dataframe(fruityvice_normalized)
+
+def get_fruit_load_list():
+      with my_cnx.cursor() as my_cur:
+           my_cur.execute("select * from fruit_load_list")
+           retunr my_cur.fetchall()
+ #add a button to load the fruit list
+if streamlit.button('Get Fruit Load List'):
+      my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+      my_data_rows = get_fruit_load_list()
+      streamlit.dataframe(my_data_rows)
+
+
 streamlit.stop()
 
 
