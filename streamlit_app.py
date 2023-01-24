e92a4fa6b5c8474ba16d7ef8a45b06aa338e6d89
Marcin Wegrzynek <121108398+Marwgithub@users.noreply.github.com>
2023-01-24 15:44:13 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,17 +62,15 @@ if streamlit.button('Get Fruit Load List'):
 
 streamlit.stop()
 
-
-
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
-#streamlit.text("The fruit load list contains:")
-#streamlit.text(my_data_row)
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+def insert_row_snowflake(new_fruit):
+      with my_cnx.cursor() as my_cur:
+           my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+           return "Thanks for adding " + new_fruit
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+if streamlit.button('Add a Fruit to the list'):
+      my_cnx = snowflake.connector.connect(**streamlit.secrests["snowflake"])
+      back_from_function = insert_row_snowlfake(add_my_fruit)
+      streamlit.text(back_from_function)
 
 #fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 #fruits_to_show = my_fruit_list.loc[fruits_selected]
